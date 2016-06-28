22a0741275d03ad9563bca5718e14122f8d2d837
Camilo Sad <camilosad@yahoo.com.br>
2016-06-28 17:04:58 -0500

    update seed script

--- db/seed.rb
+++ db/seed.rb
@@ -2,53 +2,78 @@
 
 # assuming you have ruby > 2.1 installed
 # open terminal and run 'gem install mongo'
-# run 'mongod'
 # remove comment tags according to what you want to do
-# in a new terminal tab, nivigate to db folder and run 'ruby seed.rb' to execute this code
+# navigate to this file's folder and run 'ruby seed.rb' to execute the script
 
 require 'mongo'
 require 'date'
 include Mongo
 
 
-mongo_client = Mongo::Client.new('mongodb://ds015902.mlab.com:15902',
-  database: 'heroku_7zq6pndf',
-  user: 'heroku_7zq6pndf',
-  password: 'vhr36uo60e152jsi3mkbkq8r40')
+mongo_client = Mongo::Client.new(['127.0.0.1'], database: 'iit-project')
 
 mongo_client.database.drop
 measures = mongo_client[:measures]
 
-devices = ['device1', 'device2', 'device3']
+rooms = ['TS-2030', 'TS-2033']
 
-# populate measures collection with 20 records for each device and the current datetime
-devices.each do |device|
-  20.times do |index|
-    record = {device: device, temperature: Random.new.rand(0.1..50.1),
+# insert into measures 30 records for each room (30 different days)
+rooms.each do |room|
+  30.times do |index|
+    record = {room: room,
+     temperature: Random.new.rand(0.1..50.1),
      pressure: Random.new.rand(0.1..50.1),
      humidity: Random.new.rand(0.1..50.1),
-     datetime: DateTime.now - (index * 2)}
-
+     datetime: (DateTime.now + (index)).to_time
+    }
     measures.insert_one(record)
   end
+end
+
+
+# insert into measures 10 records for each room (with different times on the same day <current day>)
+rooms.each do |room|
+    10.times do |index|
+        record = {room: room,
+         temperature: Random.new.rand(0.1..50.1),
+         pressure: Random.new.rand(0.1..50.1),
+         humidity: Random.new.rand(0.1..50.1),
+         datetime: Date.today.to_time + (index * 60 * 60)
+        }
+        measures.insert_one(record)
+    end
+end
+
+
+# insert into measures 12 records for each room (one per month)
+rooms.each do |room|
+    12.times do |index|
+        record = {room: room,
+         temperature: Random.new.rand(0.1..50.1),
+         pressure: Random.new.rand(0.1..50.1),
+         humidity: Random.new.rand(0.1..50.1),
+         datetime: Date.new(2016, index + 1, 1).to_time
+        }
+        measures.insert_one(record)
+    end
 end
 
 # print number of records in measures collection
 puts 'Number of records: ' + measures.find().count.to_s
 
-# get all measures where device = device1
-# d1_measures = measures.find(device: 'device1')
+# get all measures where room = TS-2033
+# r1_measures = measures.find(room: 'TS-2033')
 
 # print each one of those measures
-# d1_measures.each do |m|
+# r1_measures.each do |m|
 #   puts m
 # end
 
-# print the number of records from device1
-# puts d1_measures.count
+# print the number of records from room TS-2033
+# puts r1_measures.count
 
-# delete measures from device1
-# deleted_records = measures.delete_many(device: 'device1')
+# delete measures from room TS-2033
+# deleted_records = measures.delete_many(room: 'TS-2033')
 # puts deleted_records.n
 
 # print all collections in the database
